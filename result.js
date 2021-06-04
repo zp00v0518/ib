b38c3bb8ed24d761ef963876bf576d7edd4246e7
Dmitry <dmitry.tarabanov@decimadigital.com>
2021-06-04 14:50:37 +0300

    asdf

--- result.js
+++ result.js
@@ -1,10 +1,11 @@
-const cash = 1300
-const items = 13;
-const startSum = cash/items
-const persent = 0.01
-const steps = 1000
-const result = Math.floor(startSum * Math.pow(1 + persent, steps)-100)
-const grow  = ((cash+result) / cash * 100 - 100)
+const cash = 3000
+const items = 1;
+const startSum = 1000
+// const startSum = cash/items
+const persent = 0.35
+const steps = 10
+const result = Math.floor(startSum * Math.pow(1 + persent, steps))
+const grow  = (result /  startSum) * 100 - 100
 console.log(l(result), '********', l(grow)+'%');
 
 function l(value){
